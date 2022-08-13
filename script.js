dbf3ae0b65663934f6afadc863b7282025fccc89
bmilcs <bmilcs@yahoo.com>
2022-08-13 15:24:05 -0400

    Add basic math function operators

---
+++ script.js
@@ -0,0 +1,19 @@
+//
+// basic math function operators
+//
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
