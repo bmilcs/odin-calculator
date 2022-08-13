f3c29cbe28249502689385a246213625a45d0b4d
bmilcs <bmilcs@yahoo.com>
2022-08-13 18:22:49 -0400

    Add pseudocode plan of attack for operator/equal buttons

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+//
+// Variables
+//
+const buttons = Array.from(document.getElementsByTagName("button"));
+const display = document.getElementsByClassName("calculation-bar")[0];
+let currentDisplay = 0;
+let operations = [];
+
+//
+// EventListeners
+//
+
+// TODO
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //
+    console.log(e.target.getAttribute("data-value"));
+
+    if (e.target.textContent === "C") {
+      // clear button
+      currentDisplay = "";
+      operations = [];
+    } else if (
+      e.target.textContent === "*" ||
+      e.target.textContent === "+" ||
+      e.target.textContent === "-" ||
+      e.target.textContent === "/"
+    ) {
+      // On operator press +-/*
+      // if array array [0] = number && [1] = old operator
+      //      push number to array [2]
+      //      answer = operate([1], [0], [2]);
+      //      array.splice(0,2, answer, new operator)
+      //      display result
+      // else
+      //   array.push number [0]
+      //   array.push operator [1]
+      //   clear display
+    } else if (e.target.textContent === "=") {
+      // On equal press =
+      // if array.length !== 2, throw error, return
+      // push number to array [2]
+      // answer = operate([1], [0], [2]);
+      // array.splice(0,2, answer, new operator)
+      // show results in display
+    } else {
+      // clear screen
+      currentDisplay += e.target.textContent;
+    }
+
+    display.textContent = currentDisplay;
+  });
+});
+
+//
+// event listeners
+//
+
 // basic math function operators
 
 function add(x, y) {
@@ -21,5 +80,3 @@ function divide(x, y) {
 function operate(operator, x, y) {
   return window[operator](x, y);
 }
-
-console.log(operate("multiply", 5, 10));
